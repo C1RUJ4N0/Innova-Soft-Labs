a7838de09cefac7a5998432364136d82bbb17d5c
Adrian <adrian.worpechowski@educacionadventista.org.ar>
2024-06-12 14:42:19 -0300

    subida de archivos index.php

---
+++ trabajos_varios/principio.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Suma de Elementos</title>
+    </head>
+    <body>
+        <?php
+        $num1 = 8589;
+        $num2 = 3467;
+
+        $suma = $num1 + $num2;
+
+        echo '<h1>La suma es: ' .$suma. '</h1>';
+        ?>
+    </body>
+</html>
